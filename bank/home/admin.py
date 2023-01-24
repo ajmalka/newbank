f7b2d57e3a2f79fb052727ea42aed29a9b6cbdb4
ajmalka <101240233+ajmalka@users.noreply.github.com>
2023-01-24 17:38:53 +0530

    Delete admin.py

--- bank/home/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . models import District,Branch,Form,Account,Card
-# Register your models here.
-admin.site.register(District)
-admin.site.register(Branch)
-admin.site.register(Form)
-admin.site.register(Account)
-admin.site.register(Card)
