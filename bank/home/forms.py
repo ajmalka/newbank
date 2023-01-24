2bcec94bb5ce7715419322ea4de457d36b25b36f
ajmalka <101240233+ajmalka@users.noreply.github.com>
2023-01-24 17:35:36 +0530

    Delete forms.py

--- bank/home/forms.py
+++
@@ -1,7 +0,0 @@
-from .models import Form
-from django import forms
-
-class regforms(forms.Form):
-    class Meta:
-        model = Form
-        fields = ['name','dob','age','gender','phone','mail','address','district','branch','accout','card']
